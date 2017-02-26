28b72fbacf0311a31638bf478137b848b8ad73bb
Isaac McGaha Miller <isaacmcgahamiller@gmail.com>
2017-02-26 13:19:06 -0700

    added Album class, repository, and controller for persisting data article

---
+++ src/main/java/com/example/AlbumController.java
@@ -0,0 +1,28 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/albums")
+public class AlbumController {
+
+    private final AlbumRepository repository;
+
+    public AlbumController(AlbumRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("")
+    public Iterable<Album> list() {
+        return this.repository.findAll();
+    }
+
+    @PostMapping("")
+    public Album create(@RequestBody Album album) {
+        this.repository.save(album);
+        return album;
+    }
+
+}
---
+++ src/main/java/com/example/AlbumRepository.java
@@ -0,0 +1,8 @@
+package com.example;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface AlbumRepository extends CrudRepository<Album, Long> {
+
+
+}
